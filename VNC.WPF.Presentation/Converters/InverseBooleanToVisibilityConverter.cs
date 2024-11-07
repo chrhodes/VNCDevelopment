045bbe7ba113f903e456eaff3117d3d4cff075df
Christopher Rhodes <chrhodes@cox.net>
2024-11-07 12:58:41 -0800

    Moving to AdvancdeServo before coming back to InterfaceKit

---
+++ VNC.WPF.Presentation/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace VNC.WPF.Presentation.Converters
+{
+    /// <summary>
+    /// Value converter that translates true to <see cref="Visibility.Collapsed"/> 
+    /// and false to<see cref="Visibility.Visible"/>.
+    /// </summary>
+    public sealed class InverseBooleanToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool && (bool)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culturee)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
